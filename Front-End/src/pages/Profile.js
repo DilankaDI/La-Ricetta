892bc7fbd9b884e427b47b6c655f3381e537aa86
Sahani99 <sahanirandula99721@gmail.com>
2025-10-26 21:45:48 +0530

    update profile.js

--- Front-End/src/pages/Profile.js
+++ Front-End/src/pages/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Profile.css";
+import "./profile.css";
 import { FaStar, FaRegBookmark, FaBookmark } from "react-icons/fa";
 
 const Profile = () => {
